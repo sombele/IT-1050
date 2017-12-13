950f79f495a66097442b2e2e4578b714b0a158e3
sombele <31907389+sombele@users.noreply.github.com>
2017-12-12 23:28:24 -0500

    Lab 2

---
+++ Lab 2/Lab 2/Person.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    class Person
+    {
+        public int Age;
+        public string FirstName;
+        public string LastName;
+
+        public Person Spouse;
+
+
+        public static double SumOfAllAges;
+
+
+        public string GetFullName()
+        {
+            return this.FirstName + ", " + this.LastName;
+        }
+
+
+        public void AskUserForNameAndAge()
+        {
+            System.Console.Write("Enter Your First Name               : ");
+            this.FirstName = System.Console.ReadLine();
+
+            System.Console.Write("Enter Your Last Name                : ");
+            this.LastName = System.Console.ReadLine();
+
+            System.Console.Write("Enter Your Age                      : ");
+            this.Age = int.Parse(System.Console.ReadLine());
+
+            this.Spouse = new Person();
+            System.Console.Write("Enter Your Spouse's First Name      : ");
+            this.Spouse.FirstName = System.Console.ReadLine();
+
+            System.Console.Write("Enter Your Spouse's Age             : ");
+            this.Spouse.Age = int.Parse(System.Console.ReadLine());
+            this.Spouse.LastName = this.LastName;
+            Person.SumOfAllAges += this.Age + this.Spouse.Age;
+
+        }
+
+
+        public void PrintNameAndAge()
+        {
+            System.Console.WriteLine(this.GetFullName() + "   " + (Age));
+            System.Console.WriteLine(this.Spouse.GetFullName() + "   " + (Age));
+            System.Console.WriteLine("");
+        }
+
+        public static double AverageAge()
+        {
+            return (double)Person.SumOfAllAges / 4;
+        }
+    }
+}
+
+
